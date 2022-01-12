be2929df147194f05409cd5537c8b4782cf00f47
Lukas Alvarez <53347228+lukasalvarezdev@users.noreply.github.com>
2022-01-11 21:00:33 -0500

    implement delete joke

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,28 +1,60 @@
-import { useLoaderData, useParams, Link, useCatch } from 'remix'
-import type { LoaderFunction } from 'remix'
+import { useLoaderData, useParams, Link, useCatch, redirect } from 'remix'
+import type { ActionFunction } from 'remix'
 import { Joke } from '.prisma/client'
 import { db } from '~/utils/db.server'
+import type { LoaderFunctionType } from '~/utils/types'
+import { getUserId, requireUserId } from '~/utils/session.server'
 
-type LoaderData = Pick<Joke, 'name' | 'content'>
+type LoaderData = { joke: Pick<Joke, 'name' | 'content' | 'jokesterId'>; canDelete: boolean }
 
-export const loader: LoaderFunction = async ({ params }) => {
+export const loader: LoaderFunctionType<LoaderData> = async ({ params, request }) => {
   const joke = await db.joke.findUnique({
     where: { id: params.jokeId },
-    select: { content: true, name: true },
+    select: { content: true, name: true, jokesterId: true },
   })
+  const userId = await getUserId(request)
 
   if (!joke) throw new Response('Joke not found', { status: 404 })
-  return joke
+  return { joke, canDelete: joke.jokesterId === userId }
+}
+
+export const action: ActionFunction = async ({ request, params }) => {
+  const form = await request.formData()
+
+  if (form.get('_method') === 'delete') {
+    const userId = await requireUserId(request)
+    const joke = await db.joke.findUnique({
+      where: { id: params.jokeId },
+    })
+    if (!joke) {
+      throw new Response("Can't delete what does not exist", { status: 404 })
+    }
+    if (joke.jokesterId !== userId) {
+      throw new Response("Pssh, nice try. That's not your joke", {
+        status: 401,
+      })
+    }
+
+    await db.joke.delete({ where: { id: params.jokeId } })
+    return redirect('/jokes')
+  }
 }
 
 export default function JokeRoute() {
-  const joke = useLoaderData<LoaderData>()
+  const { joke, canDelete } = useLoaderData<LoaderData>()
 
   return (
     <div>
       <p>Here's your hilarious joke:</p>
       <h3>{joke.name}</h3>
       <p>{joke.content}</p>
+
+      {canDelete ? (
+        <form method="post">
+          <input type="hidden" name="_method" value="delete" />
+          <button type="submit">Delete</button>
+        </form>
+      ) : null}
     </div>
   )
 }
